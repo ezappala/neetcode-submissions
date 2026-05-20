41dbdb9177a86c93265f30f53b64321d05cf561e
Éloïse Zappala <eloise.zappala@proton.me>
2026-05-19 21:34:41 -0400

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.rs
@@ -0,0 +1,16 @@
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn has_duplicate(nums: Vec<i32>) -> bool {
+        let mut hset = HashSet::<&i32>::new();
+        let res = nums.iter().try_for_each(|n: &i32| {
+                if hset.insert(n) {
+                    Ok(())
+                } else {
+                    Err(())
+                }
+            }
+        );
+        res.is_err()
+    }
+}
