29048b572aed1538639615e2684cc4f8a68a5708
Éloïse Zappala <eloise.zappala@proton.me>
2026-07-18 15:04:13 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,29 @@
+#include <unordered_map>
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        std::unordered_map<char, int32_t> a{};
+        for (const auto& c : s) {
+            if (const auto& exists = a.emplace(c, 1); !exists.second) {
+                a[c] += 1;
+            }
+        }
+
+        std::unordered_map<char, int32_t> b{};
+        for (const auto& c : t) {
+            if (const auto& exists = b.emplace(c, 1); !exists.second) {
+                b[c] += 1;
+            }
+        }
+
+        for (const auto& [k, v] : a) {
+            if (!b.contains(k) || b[k] != v) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
