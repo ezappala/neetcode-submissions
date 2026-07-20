a08308e2dfcb1945c14381f81612d5b732b98f4a
Éloïse Zappala <eloise.zappala@proton.me>
2026-07-20 12:35:57 -0400

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.cpp
@@ -0,0 +1,22 @@
+#include <unordered_map>
+#include <map>
+#include <ranges>
+#include <algorithm>
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        std::unordered_map<int, int> fq;
+        for (const auto& num : nums) {
+            if (auto [_iter, was_inserted] = fq.try_emplace(num, 1); !was_inserted) {
+                fq[num] += 1;
+            }
+        }
+
+        auto vec = std::vector<std::pair<int, int>>(fq.begin(), fq.end());
+        std::ranges::sort(vec, std::ranges::greater(), &std::pair<int, int>::second);
+        return vec | std::views::keys 
+            | std::views::take(k) 
+            | std::ranges::to<std::vector>();
+    }
+};
